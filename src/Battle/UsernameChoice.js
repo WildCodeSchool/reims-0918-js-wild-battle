6d966b25dafc523915810788fa8c983b605ceae0
Mathieu THOMAS <kawacke@gmail.com>
2018-10-03 16:31:21 +0200

    Hover button(icons) Check

--- src/Battle/UsernameChoice.js
+++ src/Battle/UsernameChoice.js
@@ -1,39 +1,59 @@
-import React from "react";
+import React, { Component } from "react";
 import { Row, Col, Form, FormGroup, Input } from "reactstrap";
+import "./UsernameChoice.css";
 
-const UsernameChoice = () => (
-  <Row Style="height: 100vh;">
-    <Col xs={{ size: 4, offset: 1 }} className="my-auto">
-      <h3>Player 1</h3>
-      <Form>
-        <FormGroup className="d-flex">
-          <Input
-            type="text"
-            name="nickname_player_1"
-            id="player_1"
-            placeholder="Player 1 : Enter Your Nickname"
-            className="mr-2"
-          />
-          <i className="far fa-check-circle fa-2x text-success" />
-        </FormGroup>
-      </Form>
-    </Col>
-    <Col xs={{ size: 4, offset: 2 }} className="my-auto">
-      <h3>Player 2</h3>
-      <Form>
-        <FormGroup className="d-flex">
-          <Input
-            type="text"
-            name="nickname_player_2"
-            id="player_2"
-            placeholder="Player 2 : Enter Your Nickname"
-            className="mr-2"
-          />
-          <i className="fas fa-check-circle fa-2x text-success" />
-        </FormGroup>
-      </Form>
-    </Col>
-  </Row>
-);
+class UsernameChoice extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      player_1: "",
+      player_2: ""
+    };
+    this.handleChangeNickname = this.handleChangeNickname.bind(this);
+  }
+
+  handleChangeNickname(event) {
+    this.setState({ [event.target.id]: event.target.value });
+  }
+
+  render() {
+    return (
+      <Row Style="height: 100vh;">
+        <Col xs={{ size: 4, offset: 1 }} className="my-auto">
+          <h3>Player 1 {`: ${this.state.player_1}`}</h3>
+          <Form>
+            <FormGroup className="d-flex">
+              <Input
+                type="text"
+                name="nickname_player_1"
+                id="player_1"
+                placeholder="Enter Your Nickname"
+                className="mr-2"
+                onChange={this.handleChangeNickname}
+              />
+              <i className="far fa-check-circle fa-2x btnNickname" />
+            </FormGroup>
+          </Form>
+        </Col>
+        <Col xs={{ size: 4, offset: 2 }} className="my-auto">
+          <h3>Player 2 {`: ${this.state.player_2}`}</h3>
+          <Form>
+            <FormGroup className="d-flex">
+              <Input
+                type="text"
+                name="nickname_player_2"
+                id="player_2"
+                placeholder="Enter Your Nickname"
+                className="mr-2"
+                onChange={this.handleChangeNickname}
+              />
+              <i className="far fa-check-circle fa-2x btnNickname" />
+            </FormGroup>
+          </Form>
+        </Col>
+      </Row>
+    );
+  }
+}
 
 export default UsernameChoice;
