f63482c9eafea4ca4cd8e2ed54286af5e218f29c
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-10-03 12:05:14 +0200

    color removed

--- src/StatsSection/RankingTable.js
+++ src/StatsSection/RankingTable.js
@@ -11,56 +11,56 @@ const RankingTable = () => (
   <div>
     <h1 className="text-center">RANKING</h1>
     <ListGroup className="text-center h5">
-      <ListGroupItem className="border-dark" color="primary"> {/* Color not definitive */}
+      <ListGroupItem className="border-dark">
         <ListGroupItemHeading>Florentin</ListGroupItemHeading>
-        <div className="d-flex justify-content-around"> {/* d-flex for to separate win and lose */}
+        <div className="d-flex justify-content-around">
           <ListGroupItemText>Win: 7</ListGroupItemText>
           <ListGroupItemText>Lose: 3</ListGroupItemText>
         </div>
       </ListGroupItem>
-      <ListGroupItem className="border-dark" color="success">
+      <ListGroupItem className="border-dark">
         <ListGroupItemHeading>Nova</ListGroupItemHeading>
         <div className="d-flex justify-content-around">
           <ListGroupItemText>Win: 7</ListGroupItemText>
           <ListGroupItemText>Lose: 3</ListGroupItemText>
         </div>
       </ListGroupItem>
-      <ListGroupItem className="border-dark" color="info">
+      <ListGroupItem className="border-dark">
         <ListGroupItemHeading>MathIeu</ListGroupItemHeading>
         <div className="d-flex justify-content-around">
           <ListGroupItemText>Win: 7</ListGroupItemText>
           <ListGroupItemText>Lose: 3</ListGroupItemText>
         </div>
       </ListGroupItem>
-      <ListGroupItem className="border-dark" color="warning">
+      <ListGroupItem className="border-dark">
         <ListGroupItemHeading>xX FaBien Xx</ListGroupItemHeading>
         <div className="d-flex justify-content-around">
           <ListGroupItemText>Win: 7</ListGroupItemText>
           <ListGroupItemText>Lose: 3</ListGroupItemText>
         </div>
       </ListGroupItem>
-      <ListGroupItem className="border-dark" color="danger"> {/* i know, not "danger" in code but it's temporary */}
+      <ListGroupItem className="border-dark">
         <ListGroupItemHeading>My name's Charles</ListGroupItemHeading>
         <div className="d-flex justify-content-around">
           <ListGroupItemText>Win: 7</ListGroupItemText>
           <ListGroupItemText>Lose: 3</ListGroupItemText>
         </div>
       </ListGroupItem>
-      <ListGroupItem className="border-dark" color="success">
+      <ListGroupItem className="border-dark">
         <ListGroupItemHeading>King Quentin</ListGroupItemHeading>
         <div className="d-flex justify-content-around">
           <ListGroupItemText>Win: 7</ListGroupItemText>
           <ListGroupItemText>Lose: 3</ListGroupItemText>
         </div>
       </ListGroupItem>
-      <ListGroupItem className="border-dark" color="info">
+      <ListGroupItem className="border-dark">
         <ListGroupItemHeading>Pseudoless</ListGroupItemHeading>
         <div className="d-flex justify-content-around">
           <ListGroupItemText>Win: 7</ListGroupItemText>
           <ListGroupItemText>Lose: 3</ListGroupItemText>
         </div>
       </ListGroupItem>
-      <ListGroupItem className="border-dark" color="warning">
+      <ListGroupItem className="border-dark">
         <ListGroupItemHeading>Dark Noob</ListGroupItemHeading>
         <div className="d-flex justify-content-around">
           <ListGroupItemText>Win: 7</ListGroupItemText>
