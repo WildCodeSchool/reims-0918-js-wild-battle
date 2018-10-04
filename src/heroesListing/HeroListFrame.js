329920d511dcb251ab5ed909f24ba2970c0ce41f
Florentin C <florentin.coyard@gmail.com>
2018-10-04 19:36:54 +0200

    I added images to CardsListing, It has yet to be dynamic

--- src/heroesListing/HeroListFrame.js
+++ src/heroesListing/HeroListFrame.js
@@ -1,17 +1,12 @@
 import React from "react";
 import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
 
-const HeroListFrame = () => (
+const HeroListFrame = ({ imageHero, nameHero }) => (
   <a id="1" href="#">
     <Card>
-      <CardImg
-        top
-        width="100%"
-        src="https://via.placeholder.com/150x150"
-        alt="Card image cap"
-      />
+      <CardImg top width="100%" src={imageHero} alt="Card image cap" />
       <CardBody>
-        <CardTitle>Super Hero Name</CardTitle>
+        <CardTitle>{nameHero}</CardTitle>
       </CardBody>
     </Card>
   </a>
