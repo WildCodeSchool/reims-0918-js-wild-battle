bd9e67e98d29d021da30a6b66a40a932988a8ffd
Florentin C <florentin.coyard@gmail.com>
2018-10-06 17:00:33 +0200

    commit before pull

--- src/heroesListing/HeroesListing.js
+++ src/heroesListing/HeroesListing.js
@@ -4,6 +4,127 @@ import HeroListFrame from "./HeroListFrame";
 import HeroCard from "../HeroCard";
 import HeroBiography from "./HeroBiography";
 
+const heroesDB = [
+  {
+    id: "1",
+    nameHero: "Batman",
+    imageHero: "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg",
+    str: "26",
+    wise: "100",
+    speed: "27",
+    durability: "50"
+  },
+  {
+    id: "2",
+    nameHero: "Captain America",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/274.jpg?t=1476623431",
+    str: "20",
+    wise: "85",
+    speed: "40",
+    durability: "55"
+  },
+  {
+    id: "3",
+    nameHero: "DeadPool",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/835.jpg?t=1454090326",
+    str: "35",
+    wise: "85",
+    speed: "50",
+    durability: "100"
+  },
+  {
+    id: "4",
+    nameHero: "Spooder-Man",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/133.jpg?t=1476386123",
+    str: "55",
+    wise: "100",
+    speed: "70",
+    durability: "75"
+  },
+  {
+    id: "5",
+    nameHero: "Superman",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/791.jpg?t=1492625895",
+    str: "100",
+    wise: "100",
+    speed: "100",
+    durability: "100"
+  },
+  {
+    id: "6",
+    nameHero: "Wolverine",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/161.jpg?t=1490454246",
+    str: "35",
+    wise: "85",
+    speed: "50",
+    durability: "100"
+  },
+  {
+    id: "1",
+    nameHero: "Batman",
+    imageHero: "https://www.superherodb.com/pictures2/portraits/10/100/639.jpg",
+    str: "26",
+    wise: "100",
+    speed: "27",
+    durability: "50"
+  },
+  {
+    id: "2",
+    nameHero: "Captain America",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/274.jpg?t=1476623431",
+    str: "20",
+    wise: "85",
+    speed: "40",
+    durability: "55"
+  },
+  {
+    id: "3",
+    nameHero: "DeadPool",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/835.jpg?t=1454090326",
+    str: "35",
+    wise: "85",
+    speed: "50",
+    durability: "100"
+  },
+  {
+    id: "4",
+    nameHero: "Spooder-Man",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/133.jpg?t=1476386123",
+    str: "55",
+    wise: "100",
+    speed: "70",
+    durability: "75"
+  },
+  {
+    id: "5",
+    nameHero: "Superman",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/791.jpg?t=1492625895",
+    str: "100",
+    wise: "100",
+    speed: "100",
+    durability: "100"
+  },
+  {
+    id: "6",
+    nameHero: "Wolverine",
+    imageHero:
+      "https://www.superherodb.com/pictures2/portraits/10/100/161.jpg?t=1490454246",
+    str: "35",
+    wise: "85",
+    speed: "50",
+    durability: "100"
+  }
+];
+
 class HeroesListing extends Component {
   constructor(props) {
     super(props);
@@ -50,54 +171,15 @@ class HeroesListing extends Component {
           </Row>
         </Collapse>
         <Row>
-          <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
-            <HeroListFrame
-              imageHero="https://www.superherodb.com/pictures2/portraits/10/100/835.jpg?t=1454090326"
-              nameHero="DeadPool"
-            />
-          </Col>
-          <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
-            <HeroListFrame
-              imageHero="https://www.superherodb.com/pictures2/portraits/10/100/133.jpg?t=1476386123"
-              nameHero="Superman"
-            />
-          </Col>
-          <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
-            <HeroListFrame
-              imageHero="https://www.superherodb.com/pictures2/portraits/10/100/639.jpg?t=1503058475"
-              nameHero="Batman"
-            />
-          </Col>
-          <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
-            <HeroListFrame
-              imageHero="https://www.superherodb.com/pictures2/portraits/10/100/274.jpg?t=1476623431"
-              nameHero="Captain America"
-            />
-          </Col>
-          <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
-            <HeroListFrame
-              imageHero="https://www.superherodb.com/pictures2/portraits/10/100/791.jpg?t=1492625895"
-              nameHero="Superman"
-            />
-          </Col>
-          <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
-            <HeroListFrame
-              imageHero="https://www.superherodb.com/pictures2/portraits/10/100/1336.jpg?t=1445541108"
-              nameHero="Hit-Girl"
-            />
-          </Col>
-          <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
-            <HeroListFrame
-              imageHero="https://www.superherodb.com/pictures2/portraits/10/100/78.jpg?t=1341043389"
-              nameHero="Hercules"
-            />
-          </Col>
-          <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
-            <HeroListFrame
-              imageHero="https://www.superherodb.com/pictures2/portraits/10/100/83.jpg?t=1490271049"
-              nameHero="Hulk"
-            />
-          </Col>
+          {heroesDB.map(hero => (
+            <Col onClick={this.toggle} className="my-3" xs="4" md="3" lg="2">
+              <HeroListFrame
+                id={hero.id}
+                imageHero={hero.imageHero}
+                nameHero={hero.nameHero}
+              />
+            </Col>
+          ))}
         </Row>
       </section>
     );
