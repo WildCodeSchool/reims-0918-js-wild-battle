303e9ba967881318dd7e7de28605f2c0de1673d4
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-10-08 14:44:47 +0200

    removed battleScreen

--- src/App.js
+++ src/App.js
@@ -8,35 +8,9 @@ import Header from "./Header";
 import HomeNav from "./HomeNav";
 import Footer from "./Footer";
 import UsernameChoice from "./battle/UsernameChoice";
-import BattleScreen from "./battle/BattleScreen";
 import CombatInit from "./battle/CombatInit";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      battleScreen: {
-        whoPlayedRound: "Player 1",
-        player1: "Player 1",
-        player2: "Player 2",
-        round: 1
-      }
-    };
-    this.finishRoom = this.finishRoom.bind(this);
-  }
-
-  finishRoom(whoPlayed) {
-    whoPlayed === this.state.player1
-      ? this.setState({ whoPlayedRound: this.state.player2 })
-      : this.setState({
-          whoPlayedRound: this.state.player1,
-          round:
-            this.state.whoPlayedRound === this.state.player1
-              ? this.state.round
-              : this.state.round + 1
-        });
-    console.log(whoPlayed);
-  }
 
   render() {
     return (
@@ -45,10 +19,6 @@ class App extends Component {
         <Container fluid>
           <HomeNav />
           <UsernameChoice />
-          <BattleScreen
-            {...this.state.battleScreen}
-            finishRoom={this.finishRoom}
-          />
           <HeroesListing />
           <StatsSection />
           <CombatInit />
