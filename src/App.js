3255e039a8c0a393627113d58a82eeb3b65ef6e3
Florentin C <florentin.coyard@gmail.com>
2018-10-09 12:02:49 +0200

    I added the API, and 15 heroes to call

--- src/App.js
+++ src/App.js
@@ -10,7 +10,48 @@ import Footer from "./Footer";
 import UsernameChoice from "./battle/UsernameChoice";
 import CombatInit from "./battle/CombatInit";
 
+const listHeroes = [
+  30,
+  69,
+  165,
+  207,
+  222,
+  263,
+  310,
+  313,
+  322,
+  341,
+  346,
+  354,
+  514,
+  620,
+  644
+];
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      heroes: []
+    };
+  }
+
+  callApiSuperHeroes() {
+    for (let i = 0; i < listHeroes.length; i++) {
+      fetch(`http://superheroapi.com/api.php/2368931693133321/${listHeroes[i]}`)
+        .then(results => results.json()) // conversion du résultat en JSON
+        .then(data => {
+          this.setState({
+            heroes: [...this.state.heroes, data]
+          });
+        });
+    }
+  }
+
+  componentDidMount() {
+    this.callApiSuperHeroes();
+  }
+
   render() {
     return (
       <div>
@@ -19,6 +60,9 @@ class App extends Component {
           <HomeNav />
           <UsernameChoice />
           <HeroesListing />
+          <button onClick={() => this.callApiSuperHeroes()}>
+            JE GROSSIS LE BOUTON
+          </button>
           <StatsSection />
           <CombatInit />
         </Container>
