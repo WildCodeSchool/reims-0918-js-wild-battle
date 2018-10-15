0d20dcd2c08fc752489571f6a6e299d4b44ffaa4
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-10-15 16:37:08 +0200

    BattleContext

--- src/heroesListing/HeroListFrame.js
+++ src/heroesListing/HeroListFrame.js
@@ -1,16 +1,24 @@
 import React from "react";
 import { Card, CardImg, CardBody, CardTitle } from "reactstrap";
 import Scrollchor from "react-scrollchor";
+import BattleContext from "../battle_context/BattleContext";
 
-const HeroListFrame = ({ imageHero, nameHero, toggle, heroData }) => (
-  <Scrollchor to="#heroesListSection" beforeAnimate={() => toggle(heroData)}>
-    <Card>
-      <CardImg top width="100%" src={imageHero} alt="Card image cap" />
-      <CardBody>
-        <CardTitle className="cardTitleHero">{nameHero} </CardTitle>
-      </CardBody>
-    </Card>
-  </Scrollchor>
+const HeroListFrame = ({ imageHero, nameHero, heroData }) => (
+  <BattleContext.Consumer>
+    {(siteContext) => (
+      <Scrollchor
+        to="#heroesListSection"
+        beforeAnimate={() => siteContext.toggle(heroData)}
+      >
+        <Card>
+          <CardImg top width="100%" src={imageHero} alt="Card image cap" />
+          <CardBody>
+            <CardTitle className="cardTitleHero">{nameHero} </CardTitle>
+          </CardBody>
+        </Card>
+      </Scrollchor>
+    )}
+  </BattleContext.Consumer>
 );
 
 export default HeroListFrame;
