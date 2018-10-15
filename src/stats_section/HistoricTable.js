6f59d818f5c5a9295b5390bf671558f7dc89204b
Michaël BOURMEAU <michael.bourmeau@gmail.com>
2018-10-15 09:47:03 +0200

    json added

--- src/stats_section/HistoricTable.js
+++ src/stats_section/HistoricTable.js
@@ -1,6 +1,6 @@
 import React from "react";
-import BackgroundHistoric from "../img/greenred.png"
-import "./HistoricTable.css"
+import BackgroundHistoric from "../img/greenred.png";
+import "./HistoricTable.css";
 
 import {
   ListGroup,
@@ -9,89 +9,14 @@ import {
   CardTitle,
   CardText,
   CardImg,
-  CardImgOverlay
+  CardImgOverlay,
 } from "reactstrap";
-
-const battleHistoric = [
-  {
-    player1: "Florentin",
-    player2: "Mathieu",
-    id: "1",
-    date: "10/03/2018 11:15",
-    player1Score: "Win",
-    player2Score: "Lose"
-  },
-  {
-    player1: "Mathieu",
-    player2: "Florentin",
-    id: "2",
-    date: "10/03/2018 11:15",
-    player1Score: "Lose",
-    player2Score: "Win"
-  },
-  {
-    player1: "Michael",
-    player2: "Florentin",
-    id: "3",
-    date: "10/03/2018 11:15",
-    player1Score: "Lose",
-    player2Score: "Win"
-  },
-  {
-    player1: "Fabien",
-    player2: "Michael",
-    id: "4",
-    date: "10/03/2018 11:15",
-    player1Score: "Win",
-    player2Score: "Lose"
-  },
-  {
-    player1: "Thomas",
-    player2: "Clément",
-    id: "5",
-    date: "10/03/2018 11:15",
-    player1Score: "Win",
-    player2Score: "Lose"
-  },
-  {
-    player1: "Clément",
-    player2: "Romain",
-    id: "6",
-    date: "10/03/2018 11:15",
-    player1Score: "Lose",
-    player2Score: "Win"
-  },
-  {
-    player1: "Romain",
-    player2: "Thomas",
-    id: "7",
-    date: "10/03/2018 11:15",
-    player1Score: "Lose",
-    player2Score: "Win"
-  },
-  {
-    player1: "Quentin",
-    player2: "Corentin",
-    id: "8",
-    date: "10/03/2018 11:15",
-    player1Score: "Lose",
-    player2Score: "Win"
-  },
-  {
-    player1: "Charles",
-    player2: "Robin",
-    id: "9",
-    date: "10/03/2018 11:15",
-    player1Score: "Win",
-    player2Score: "Lose"
-  }
-];
 
 const HistoricTable = () => (
   <div>
     <h1 className="text-center">HISTORIC</h1>
     <ListGroup className="text-center h5">
-      {battleHistoric.map(battleData => (
+      {battleHistoric.map((battleData) => (
         <ListGroupItem className="border-dark p-0" key={battleData.id}>
           <Card inverse>
             <CardImg
@@ -99,17 +24,17 @@ const HistoricTable = () => (
               src={BackgroundHistoric}
               alt="greenred"
               height="95"
-              className={(battleData.player1Score === "Lose") ? "rotateImg" : ""}
+              className={battleData.player1Score === "Lose" ? "rotateImg" : ""}
             />
             <CardImgOverlay>
               <CardTitle>{battleData.date}</CardTitle>
               <div className="d-flex justify-content-around">
-              <CardText>
-                {battleData.player1} : {battleData.player1Score}
-              </CardText>
-              <CardText>
-                {battleData.player2} : {battleData.player2Score}
-              </CardText>
+                <CardText>
+                  {battleData.player1} : {battleData.player1Score}
+                </CardText>
+                <CardText>
+                  {battleData.player2} : {battleData.player2Score}
+                </CardText>
               </div>
             </CardImgOverlay>
           </Card>
--- src/stats_section/RankingTable.js
+++ src/stats_section/RankingTable.js
@@ -7,63 +7,6 @@ import {
   ListGroupItemText
 } from "reactstrap";
 
-const ranking = [
-  {
-    name: "Florentin",
-    id: "1",
-    win: "42",
-    lose: "0"
-  },
-  {
-    name: "Thomas",
-    id: "2",
-    win: "2",
-    lose: "0"
-  },
-  {
-    name: "Fabien",
-    id: "3",
-    win: "1",
-    lose: "0"
-  },
-  {
-    name: "Corentin",
-    id: "4",
-    win: "4",
-    lose: "0"
-  },
-  {
-    name: "Charles",
-    id: "5",
-    win: "1",
-    lose: "0"
-  },
-  {
-    name: "Romain",
-    id: "6",
-    win: "1",
-    lose: "1"
-  },
-  {
-    name: "Quentin",
-    id: "7",
-    win: "5",
-    lose: "1"
-  },
-  {
-    name: "Robin",
-    id: "8",
-    win: "0",
-    lose: "1"
-  },
-  {
-    name: "Michaël",
-    id: "9",
-    win: "50",
-    lose: "2"
-  }
-];
-
 const rank_sort = ranking =>
   ranking.sort((person1, person2) => person2.win - person1.win);
 
