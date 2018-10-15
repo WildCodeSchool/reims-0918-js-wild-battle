13dbc5e5a67744921563b8c191b426d977a94269
Fabien Raymond <voortexxx@gmail.com>
2018-10-15 16:31:29 +0200

    Context Api on CombatInit

--- src/battle_context/BattleProvider.js
+++ src/battle_context/BattleProvider.js
@@ -1,12 +1,39 @@
 import React, { Component } from "react";
 import BattleContext from "./BattleContext";
 
+const listHeroes = [
+  30,
+  69,
+  165,
+  207,
+  213,
+  222,
+  226,
+  263,
+  310,
+  313,
+  322,
+  341,
+  346,
+  354,
+  361,
+  386,
+  485,
+  514,
+  620,
+  644
+];
+
 class BattleProvider extends Component {
   state = {
     battle: {
+      stats: ["Strength", "Speed", "Intelligence", "Durability"],
+      heroes: [],
+      randomHero: [],
       player_1: {
         nickname: "",
-        nicknameChecked: false
+        nicknameChecked: false,
+        deck: []
       },
       player_2: {
         nickname: "",
@@ -15,10 +42,30 @@ class BattleProvider extends Component {
       round: {
         roundNumber: 1,
         roundStats: "",
+        randomStat: "",
         currentPlayer: "Mathieu"
       }
     }
   };
+
+  callApiSuperHeroes() {
+    for (let i = 0; i < listHeroes.length; i++) {
+      fetch(`http://superheroapi.com/api.php/2368931693133321/${listHeroes[i]}`)
+        .then(results => results.json()) // conversion du résultat en JSON
+        .then(data => {
+          this.setState({
+            battle: {
+              ...this.state.battle,
+              heroes: [...this.state.battle.heroes, data]
+            }
+          });
+        });
+    }
+  }
+
+  componentDidMount() {
+    this.callApiSuperHeroes();
+  }
 
   render() {
     return (
@@ -42,6 +89,67 @@ class BattleProvider extends Component {
                 [name]: {
                   ...this.state.battle[name],
                   nicknameChecked: true
+                }
+              }
+            });
+          },
+          selectHero: id => {
+            const deck = this.state.battle.player_1.deck.filter(
+              hero => hero.id !== id
+            );
+            const randomHero = Math.floor(
+              Math.random() * this.state.battle.heroes.length
+            );
+            this.setState({
+              battle: {
+                ...this.state.battle,
+                player_1: {
+                  ...this.state.battle.player_1,
+                  deck,
+
+                  selectedHero: this.state.battle.player_1.deck.filter(
+                    hero => hero.id === id
+                  )
+                },
+                randomHero: randomHero
+              }
+            });
+          },
+
+          getRandomInt: () => {
+            const randomInt = Math.floor(
+              Math.random() * Math.floor(this.state.battle.stats.length)
+            );
+            this.setState({
+              battle: {
+                ...this.state.battle,
+                round: {
+                  ...this.state.battle.round,
+                  randomStat: randomInt
+                }
+              }
+            });
+          },
+          getRandomDeck: () => {
+            let oneCard = 0;
+            const deck = [];
+            for (let i = 5; i > 0; i--) {
+              const randomN = Math.floor(
+                Math.random() * this.state.battle.heroes.length
+              );
+              oneCard = this.state.battle.heroes[randomN];
+              if (deck.indexOf(oneCard) === -1) {
+                deck.push(oneCard);
+              } else {
+                i++;
+              }
+            }
+            this.setState({
+              battle: {
+                ...this.state.battle,
+                player_1: {
+                  ...this.state.battle.player_1,
+                  deck: deck
                 }
               }
             });
