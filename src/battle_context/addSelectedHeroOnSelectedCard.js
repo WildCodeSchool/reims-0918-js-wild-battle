920577bdefacebe13b6924cfbf1619b899e7b37b
Mathieu THOMAS <kawacke@gmail.com>
2018-10-25 11:29:35 +0200

    Change cards selection (Used)

--- src/battle_context/addSelectedHeroOnSelectedCard.js
+++ src/battle_context/addSelectedHeroOnSelectedCard.js
@@ -1,7 +1,12 @@
 const addSelectedHeroOnSelectedCard = (prevState, idHeroSelected) => {
-  const deck = prevState.battle[
-    prevState.battle.round.currentPlayer
-  ].deck.filter(hero => hero.id !== idHeroSelected);
+  const deck = prevState.battle[prevState.battle.round.currentPlayer].deck.map(
+    hero => {
+      if (hero.id === idHeroSelected) {
+        hero.used = true;
+      }
+      return hero;
+    }
+  );
   return {
     battle: {
       ...prevState.battle,
--- src/battle_context/generateDeck.js
+++ src/battle_context/generateDeck.js
@@ -3,11 +3,13 @@ import getRandomNumber from "./getRandomNumber";
 const generateDeck = (prevState, sizeDeck) => {
   let oneCard = 0;
   const deck = [];
+  const listNumberRandom = [];
   for (let i = sizeDeck; i > 0; i--) {
     const randomN = getRandomNumber(prevState.battle.heroes.length);
-    oneCard = prevState.battle.heroes[randomN];
-    if (deck.indexOf(oneCard) === -1) {
-      deck.push(oneCard);
+    if (!listNumberRandom.includes(randomN)) {
+      oneCard = prevState.battle.heroes[randomN];
+      listNumberRandom.push(randomN);
+      deck.push({ ...oneCard });
     } else {
       i++;
     }
