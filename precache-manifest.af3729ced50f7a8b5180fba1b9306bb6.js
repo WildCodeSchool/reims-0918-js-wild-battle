97f7b46e8bd018a2ddd7774800965050276caff1
Fabien Raymond <voortexxx@gmail.com>
2018-11-12 11:20:02 +0100

    Updates

---
+++ precache-manifest.af3729ced50f7a8b5180fba1b9306bb6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "eb559b17c7e3db2d8a2c4746b2d832e3",
+    "url": "/reims-0918-js-wild-battle/static/media/Clement.eb559b17.png"
+  },
+  {
+    "revision": "edf900dc1c462609affb",
+    "url": "/reims-0918-js-wild-battle/static/css/main.30486aad.chunk.css"
+  },
+  {
+    "revision": "0d2541273656e2e7bae7fb0832126da5",
+    "url": "/reims-0918-js-wild-battle/static/media/list.0d254127.png"
+  },
+  {
+    "revision": "516a921bb9593256ff0c",
+    "url": "/reims-0918-js-wild-battle/static/js/1.516a921b.chunk.js"
+  },
+  {
+    "revision": "995e4d769c35e0b36c33",
+    "url": "/reims-0918-js-wild-battle/static/js/runtime~main.995e4d76.js"
+  },
+  {
+    "revision": "f721e8eba82a9476647225c6928058a3",
+    "url": "/reims-0918-js-wild-battle/static/media/logo.f721e8eb.svg"
+  },
+  {
+    "revision": "edf900dc1c462609affb",
+    "url": "/reims-0918-js-wild-battle/static/js/main.edf900dc.chunk.js"
+  },
+  {
+    "revision": "7dd3a24e22a54242687fb5a49ae75477",
+    "url": "/reims-0918-js-wild-battle/static/media/Thomas.7dd3a24e.png"
+  },
+  {
+    "revision": "61c8ea6dc493d4da0a5e330753355215",
+    "url": "/reims-0918-js-wild-battle/static/media/circleBackground.61c8ea6d.svg"
+  },
+  {
+    "revision": "40f5cf89f1b6563fbdbb510f39f6436f",
+    "url": "/reims-0918-js-wild-battle/static/media/fight.40f5cf89.png"
+  },
+  {
+    "revision": "050f56ddb5df4ac6436497c102e58fe1",
+    "url": "/reims-0918-js-wild-battle/static/media/stats.050f56dd.png"
+  },
+  {
+    "revision": "516a921bb9593256ff0c",
+    "url": "/reims-0918-js-wild-battle/static/css/1.d38df516.chunk.css"
+  },
+  {
+    "revision": "7ce74acf2d9051997f7d532e78fb8a75",
+    "url": "/reims-0918-js-wild-battle/index.html"
+  }
+];
